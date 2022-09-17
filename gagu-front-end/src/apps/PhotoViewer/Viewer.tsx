acb638db5225eebb2cf29021c05ebc95ccf2da3e
Chisw <chisw@qq.com>
2022-09-17 21:23:36 +0800

    minor

--- gagu-front-end/src/apps/PhotoViewer/Viewer.tsx
+++ gagu-front-end/src/apps/PhotoViewer/Viewer.tsx
@@ -3,6 +3,7 @@ import { FsApi } from '../../api'
 import { SvgIcon } from '../../components/base'
 import { IconButton } from '../../components/base/IconButton'
 import { IEntry } from '../../types'
+import { getPaddedNo } from '../../utils'
 
 interface ViewerProps {
   activeIndex: number
@@ -33,24 +34,30 @@ export default function Viewer(props: ViewerProps) {
         bannerVisible={false}
         overlayRender={({ rotate, onRotate, scale, onScale, index, images, onClose }) => {
           return (
-            <div className="absolute z-10 top-0 right-0 left-0 h-10 flex items-center bg-black-500">
-              <span className="text-white font-din text-xs">{index + 1} / {images.length} {matchedEntryList[index].name}</span>
-              <IconButton
-                icon={<SvgIcon.SubtractCircle size={18} />}
-                onClick={() => onScale(scale - 1)}
-              />
-              <IconButton
-                icon={<SvgIcon.AddCircle size={18} />}
-                onClick={() => onScale(scale + 1)}
-              />
-              <IconButton
-                icon={<SvgIcon.Restart size={18} />}
-                onClick={() => onRotate(rotate + 90)}
-              />
-              <IconButton
-                icon={<SvgIcon.Close size={18} />}
-                onClick={onClose}
-              />
+            <div className="absolute z-10 top-0 right-0 left-0 pl-2 pr-1 h-10 flex justify-between items-center bg-black-500">
+              <span className="text-white font-din text-sm">
+                {getPaddedNo(activeIndex, matchedEntryList.length)}
+                &emsp;
+                {matchedEntryList[activeIndex].name}
+              </span>
+              <div className="flex items-center">
+                <IconButton
+                  icon={<SvgIcon.SubtractCircle size={18} />}
+                  onClick={() => onScale(scale - 1)}
+                />
+                <IconButton
+                  icon={<SvgIcon.AddCircle size={18} />}
+                  onClick={() => onScale(scale + 1)}
+                />
+                <IconButton
+                  icon={<SvgIcon.Restart size={18} />}
+                  onClick={() => onRotate(rotate + 90)}
+                />
+                <IconButton
+                  icon={<SvgIcon.Close size={18} />}
+                  onClick={onClose}
+                />
+              </div>
             </div>
           )
         }}
