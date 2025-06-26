850da95f106eda2f747ed58985ee0fa8c951c4e4
Chisw <chisw@qq.com>
2025-06-26 17:46:54 +0800

    add EyeDropper color picker

--- gagu-chrome-extension/popup/popup.js
+++ gagu-chrome-extension/popup/popup.js
@@ -1,5 +1,33 @@
 ;(function() {
+  const copy = (str) => {
+    const input = document.createElement('textarea')
+    document.body.appendChild(input)
+    input.value = str
+    input.select()
+    document.execCommand('Copy')
+    document.body.removeChild(input)
+    input.remove()
+  }
+
   document.querySelector('#refresh').addEventListener('click', () => {
     chrome.runtime.reload()
+  })
+
+  document.querySelector('#color-picker').addEventListener('click', async () => {
+    try {
+      const { sRGBHex } = await new EyeDropper().open()
+      copy(sRGBHex)
+
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: '../static/img/icon.png',
+        title: 'ColorPicker',
+        message: `Value copied: ${sRGBHex}`,
+        priority: 0, // 优先级，0是默认，不会弹出强制用户交互
+        // buttons: [{ title: '确定' }]
+      });
+
+      setTimeout(window.close, 200)
+    } catch(e) {}
   })
 }());
